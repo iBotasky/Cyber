c2ed631cc20e44f1a2acee7d218556e0ddc6df73
Botasky <291770865@qq.com>
2016-11-21 23:35:34 +0800

    项目初始化

---
+++ app/src/main/java/com/botasky/cyberblack/CyberApplication.java
@@ -0,0 +1,68 @@
+package com.botasky.cyberblack;
+
+import android.app.Application;
+import android.content.res.Configuration;
+
+/**
+ * Created by Botasky on 21/11/2016.
+ */
+
+public class CyberApplication extends Application {
+    /**
+     * 单例模式
+     * 优点：延迟加载，线程安全（java中class加载时互斥的），也减少了内存消耗
+     * 内部类实现单例模式
+     * 延迟加载，减少内存开销
+     *
+     * @author xuzhaohu
+     *
+     */
+    private static class SingletonHolder {
+        private static final CyberApplication instance = new CyberApplication();
+    }
+    /**
+     * 私有的构造函数
+     */
+    private CyberApplication() {}
+    public static CyberApplication getInstance() {
+        return SingletonHolder.instance;
+    }
+
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+
+
+
+    /**
+     * 作为onLowMemory的一个特定于应用程序的替代选择，在android4.0时引入，
+     * 在程序运行时决定当前应用程序应该尝试减少其内存开销时（通常在它进入后台时）调用
+     * 它包含一个level参数，用于提供请求的上下文
+     */
+    @Override
+    public void onTrimMemory(int level) {
+        super.onTrimMemory(level);
+    }
+
+    /**
+     * 与Activity不同，在配置改变时，应用程序对象不会被终止和重启。
+     * 如果应用程序使用的值依赖于特定的配置，则重写这个方法来重新加载这些值，或者在应用程序级别处理这些值的改变
+     */
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+    }
+
+    /**
+     * 当系统处于资源匮乏时，具有良好行为的应用程序可以释放额外的内存。
+     * 这个方法一般只会在后台进程已经终止，但是前台应用程序仍然缺少内存时调用。
+     * 我们可以重写这个程序来清空缓存或者释放不必要的资源
+     */
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+    }
+}

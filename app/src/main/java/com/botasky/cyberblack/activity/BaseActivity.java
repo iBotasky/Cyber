e1644af6bf6039c5e4bbfc3dbf8453827d37c427
Botasky <291770865@qq.com>
2016-11-25 23:38:13 +0800

    首页的侧滑栏

---
+++ app/src/main/java/com/botasky/cyberblack/activity/BaseActivity.java
@@ -0,0 +1,30 @@
+package com.botasky.cyberblack.activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * 项目Activity的父类
+ * Created by Botasky on 25/11/2016.
+ */
+
+public abstract class BaseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    protected abstract void initVariables();
+
+    protected abstract void initViews();
+
+    protected abstract void loadData();
+}
---
+++ app/src/main/java/com/botasky/cyberblack/activity/HomeActivity.java
@@ -0,0 +1,32 @@
+package com.botasky.cyberblack.activity;
+
+import android.os.Bundle;
+
+import com.botasky.cyberblack.R;
+
+/**
+ * Created by Botasky on 25/11/2016.
+ */
+
+public class HomeActivity extends BaseActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+
+    @Override
+    protected void initVariables() {
+
+    }
+
+    @Override
+    protected void initViews() {
+
+    }
+
+    @Override
+    protected void loadData() {
+
+    }
+}

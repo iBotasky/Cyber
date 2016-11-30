ad80dc6f83f596881d3a039a619972af0a2b9129
Botasky <291770865@qq.com>
2016-11-30 14:15:28 +0800

    splash动画

--- app/src/main/java/com/botasky/cyberblack/fragment/SplashFragment.java
+++ app/src/main/java/com/botasky/cyberblack/fragment/SplashFragment.java
@@ -1,14 +1,20 @@
 package com.botasky.cyberblack.fragment;
 
-import android.content.Context;
+import android.animation.ObjectAnimator;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.support.v7.view.ContextThemeWrapper;
+import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.botasky.cyberblack.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * Created by Botasky on 27/11/2016.
@@ -16,9 +22,35 @@ import com.botasky.cyberblack.R;
 
 public class SplashFragment extends BaseFragment {
 
+    @BindView(R.id.hello_tv_sp)
+    TextView helloTvSp;
+    @BindView(R.id.hello_tv_cn)
+    TextView helloTvCn;
+    @BindView(R.id.hello_tv_france)
+    TextView helloTvFrance;
+    @BindView(R.id.hello_tv_korea)
+    TextView helloTvKorea;
+    @BindView(R.id.hello_tv_jpan)
+    TextView helloTvJpan;
+    @BindView(R.id.hello_tv_germany)
+    TextView helloTvGermany;
+    @BindView(R.id.hello_tv_en)
+    TextView helloTvEn;
+    @BindView(R.id.hello_tv_world)
+    TextView helloTvWorld;
+    @BindView(R.id.hello_tv_russuia)
+    TextView helloTvRussuia;
+    @BindView(R.id.imageView)
+    ImageView imageView;
+
+    private Animation tvSpAnim;
+
+
     @Override
     protected void initView(View view, Bundle savedInstanceState) {
-
+        tvSpAnim = new AlphaAnimation(0.0f,1.0f);
+        tvSpAnim.setDuration(4000);
+        helloTvSp.startAnimation(tvSpAnim);
     }
 
     @Override
@@ -26,10 +58,26 @@ public class SplashFragment extends BaseFragment {
         return R.layout.fragment_splash;
     }
 
-//    @Override
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // TODO: inflate a fragment view
+        View rootView = super.onCreateView(inflater, container, savedInstanceState);
+        unbinder = ButterKnife.bind(this, rootView);
+        initView(rootView, savedInstanceState);
+        return rootView;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
+    }
+
+
+    //    @Override
 //    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 //
-        // create ContextThemeWrapper from the original Activity Context with the custom theme
+    // create ContextThemeWrapper from the original Activity Context with the custom theme
 //        final Context contextThemeWrapper = new ContextThemeWrapper(getActivity(), R.style.splash_theme);
 //
 //        // clone the inflater using the ContextThemeWrapper

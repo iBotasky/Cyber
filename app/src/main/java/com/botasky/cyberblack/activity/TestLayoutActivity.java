c1d14385d0d3857eddf97e08a3330b6cfdb5e6ce
Botasky <291770865@qq.com>
2017-02-03 10:55:49 +0800

    gradle规范

---
+++ app/src/main/java/com/botasky/cyberblack/activity/TestLayoutActivity.java
@@ -0,0 +1,15 @@
+package com.botasky.cyberblack.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.botasky.cyberblack.R;
+
+public class TestLayoutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test_layout);
+    }
+}

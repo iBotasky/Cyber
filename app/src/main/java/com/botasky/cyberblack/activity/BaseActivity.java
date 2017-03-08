bafd020f754a67ea0a267f0b7edc1e132645cde6
Botasky <291770865@qq.com>
2017-03-08 16:08:09 +0800

    Update BaseActivity add StateBar setting

--- app/src/main/java/com/botasky/cyberblack/activity/BaseActivity.java
+++ app/src/main/java/com/botasky/cyberblack/activity/BaseActivity.java
@@ -1,7 +1,9 @@
 package com.botasky.cyberblack.activity;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.WindowManager;
 
 /**
  * 项目Activity的父类
@@ -12,6 +14,9 @@ public abstract class BaseActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        }
         super.onCreate(savedInstanceState);
     }
 

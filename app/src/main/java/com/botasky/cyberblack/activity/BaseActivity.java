191de32306e3c4de0cf9174a7f6b23ffd4aac106
Botasky <291770865@qq.com>
2017-03-29 15:19:15 +0800

    Fix state bar

--- app/src/main/java/com/botasky/cyberblack/activity/BaseActivity.java
+++ app/src/main/java/com/botasky/cyberblack/activity/BaseActivity.java
@@ -1,9 +1,20 @@
 package com.botasky.cyberblack.activity;
 
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
 import android.view.WindowManager;
+
+import com.botasky.cyberblack.R;
+
+import java.lang.reflect.Field;
 
 /**
  * 项目Activity的父类
@@ -14,10 +25,12 @@ public abstract class BaseActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-//            getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
-//        }
         super.onCreate(savedInstanceState);
+        //透明状态栏，5.0系统
+        getWindow().setStatusBarColor(Color.TRANSPARENT);
+        getWindow()
+                .getDecorView()
+                .setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
     }
 
     @Override
--- app/src/main/java/com/botasky/cyberblack/activity/TestLayoutActivity.java
+++ app/src/main/java/com/botasky/cyberblack/activity/TestLayoutActivity.java
@@ -1,15 +1,53 @@
 package com.botasky.cyberblack.activity;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.ActivityOptions;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.constraint.Guideline;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
+import com.botasky.cyberblack.MainActivity;
 import com.botasky.cyberblack.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class TestLayoutActivity extends AppCompatActivity {
+
+    @BindView(R.id.login)
+    Button login;
+    @BindView(R.id.register)
+    Button register;
+    @BindView(R.id.guideline)
+    Guideline guideline;
+    @BindView(R.id.imageView2)
+    ImageView imageView2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test_layout);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick({R.id.login, R.id.register})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.login:
+                Intent intent = new Intent(this, MainActivity.class);
+                // create the transition animation - the images in the layouts
+                // of both activities are defined with android:transitionName="robot"
+                ActivityOptions options = ActivityOptions
+                        .makeSceneTransitionAnimation(this, imageView2, "hello");
+                // start the new activity
+                startActivity(intent, options.toBundle());
+                break;
+            case R.id.register:
+                break;
+        }
     }
 }
